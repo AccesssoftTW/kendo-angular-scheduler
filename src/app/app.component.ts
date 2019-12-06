5347cd6f5b15a165eeb7f55bc51059fa71d66141
Winston 盧文松 <aa71435723@gmail.com>
2019-12-06 16:23:50 +0800

    實作scheduler第一版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { SchedulerEvent, CreateFormGroupArgs } from '@progress/kendo-angular-scheduler';
+import { FormGroup, FormBuilder, Validators, ValidatorFn } from '@angular/forms';
+
+import '@progress/kendo-date-math/tz/regions/Europe';
+import '@progress/kendo-date-math/tz/regions/NorthAmerica';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kendo-angular-scheduler';
+  public selectedDate: Date = new Date('2018-10-22T00:00:00');
+  public formGroup: FormGroup;
+  public events: SchedulerEvent[] = [{
+    id: 1,
+    title: 'Breakfast',
+    start: new Date('2018-10-22T09:00:00'),
+    end: new Date('2018-10-22T09:30:00'),
+    recurrenceRule: 'FREQ=DAILY;COUNT=5;'
+  }];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createFormGroup = this.createFormGroup.bind(this);
+  }
+
+  public createFormGroup(args: CreateFormGroupArgs): FormGroup {
+    const dataItem = args.dataItem;
+
+    this.formGroup = this.formBuilder.group({
+      'id': args.isNew ? this.getNextId() : dataItem.id,
+      'start': [dataItem.start, Validators.required],
+      'end': [dataItem.end, Validators.required],
+      'startTimezone': [dataItem.startTimezone],
+      'endTimezone': [dataItem.endTimezone],
+      'isAllDay': dataItem.isAllDay,
+      'title': dataItem.title,
+      'description': dataItem.description,
+      'recurrenceRule': dataItem.recurrenceRule,
+      'recurrenceId': dataItem.recurrenceId
+    }, {
+      validator: this.startEndValidator
+    });
+
+    return this.formGroup;
+  }
+
+  public getNextId(): number {
+    const len = this.events.length;
+
+    return (len === 0) ? 1 : this.events[this.events.length - 1].id + 1;
+  }
+
+  public startEndValidator: ValidatorFn = (fg: FormGroup) => {
+    const start = fg.get('start').value;
+    const end = fg.get('end').value;
+
+    if (start !== null && end !== null && start.getTime() < end.getTime()) {
+      return null;
+    } else {
+      return { range: 'End date must be greater than Start date' };
+    }
+  }
 }
